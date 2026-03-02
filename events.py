9a3a16eb0f78ccea005840aeb62cbbf951ce53a8
Flaes2 <FlaesPlays@gmail.com>
2026-03-02 10:08:02 -0500

    Update events.py

--- events.py
+++ events.py
@@ -1,2 +1,6 @@
+import random
+
 def travel_event(player):
-  
+  if random.randint(0, 100) < 20:
+    print(!!! ALERT: UNKNOWN VESSEL SPOTTED !!! Space Pirates have intercepted your ship! They demand a 20 Credit toll.)
+    input(Do you (1) Pay the toll, (2) Attempt to outrun them, or (3) Fight?)
