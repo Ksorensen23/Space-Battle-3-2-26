d11b96ddfce676797b87e0d1d0416d2bfa728325
Zebulan the Red <zebpeak2023@gmail.com>
2026-03-03 08:42:10 -0500

    Update effects.py

--- effects.py
+++ effects.py
@@ -1,1 +1,15 @@
-def start()
+import time
+import sys
+
+def typewriter(str):
+  for char in str
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(1)
+
+def textCleanUp(input):
+  if input.isdigit():
+    retVal = "error no answer selected"
+  else:
+    retVal = input.upper()
+  return retVal
