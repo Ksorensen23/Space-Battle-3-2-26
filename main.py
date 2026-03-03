4bcc1aeff6cf82f4429a3e598474bd830f971be8
Flaes2 <FlaesPlays@gmail.com>
2026-03-03 10:31:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -120,8 +120,52 @@ def StatsBoard():
     print("*                                                     *")
     print("*******************************************************")
   #=================== 
-
-
+  # Liam's code
+def travel_event(player): # random event function
+    Atmodes = ["You fly past a dying star...", "You pass by an abandoned satellite"] # the flavour text for traveling
+    if random.randint(0, 100) < 20: # 20% chance of happening
+        typewriter("!!! ALERT: UNKNOWN VESSEL SPOTTED !!! Space Pirates have intercepted your ship! They demand a 20 Credit toll.", 0.04)
+        playerChoice = textCleanUp(input("\nDo you (F)ight, (B)ribe, or (R)un? Print player(P)")) # input for how they should act against the pirates
+        if playerChoice == "F": # they choose to fight
+            if random.randint(0, 100) < 30: # 30% chance of losing
+                creditLoss = random.randint(20, 35) # how many credits they should lose
+                typewriter(f"You attempt to defeat the pirates, but unfortunately are beaten down. You lose {creditLoss} credits", 0.04)
+                player["credits"] -= creditLoss # removes the lost credits from player inventory
+            else: # 70% chance of winning
+                typewriter("You successfully fend off the Pirates! You continue on your way.", 0.04)
+        elif playerChoice == "B": # they choose to bribe them
+            while True: # runs the amount they wish to bribe with until they get a valid input
+                creditBribe = input("How many credits do you attempt to bribe the pirates with?") # they bribe
+                if creditBribe.isdigit(): # makes sure the bribe is a number
+                    creditBribeInt = int(creditBribe) # turns it from str to int
+                    if 0 <= creditBribeInt <= player["credits"]: # do they have enough money?
+                        if random.randint(0, 20) < creditBribeInt: # this makes it so the higher the bribe, the chance of succeeding is higher
+                            typewriter(f"The pirates accept your bribe and fly away. You lose {creditBribeInt} credits.", 0.04)
+                            player["credits"] -= creditBribeInt # remove the bribe amount from inventory
+                        else: #the check fails 
+                            typewriter("The pirates don't accept your bribe and take 20 credits", 0.04)
+                            player["credits"] -= 20 # deduct 20 credits for toll
+                        break # stops the loop because they did everything right and the check was made. 
+                                # If we instead break after confirming the bribe is a valid answer, it would skip 
+                                # over the logic desiding if they convince the pirates and ho much money they lose
+                    elif creditBribeInt > player["credits"]: # is the bribe invalid because they don't have enough money?
+                        print("You don't have that much money!")
+                    else: # number can only be non positive now
+                        print("You need to put a positive number.")
+                else: # answer wasn't a number
+                    print("It needs to be a number!")
+        elif playerChoice == "R": # they choose to run
+            if random.randint(0, player["fuel"]) >= 10: # the higher their fuel, the greater change of succeeding
+                fuelLoss = random.randint(3, 10) # they lose this much fuel for running
+                typewriter(f"Rolling engines... Success! You boosted past them, but used {fuelLoss} extra Fuel.", 0.04)
+                player["fuel"] -= fuelLoss # deduct the fuel from inventory
+            else: # they lose the check for running
+                fuelLoss = random.randint(2, 6) # how much fuel they waste
+                creditLoss = random.randint(22, 30) # how much money they lose
+                typewriter(f"Rolling engines... Failure. You attempt to outrun the pirates but they catch up. You waste {fuelLoss} fuel and lose {creditLoss} credits.", 0.04)
+                player["fuel"] -= fuelLoss #deduct lost fuel
+                player["credits"] -= creditLoss #deduct lost credits
+              
 
 
 
