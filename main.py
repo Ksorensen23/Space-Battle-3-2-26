1dd1d9135653d98766f7c9e6ff74cfbd02a03516
Zebulan the Red <zebpeak2023@gmail.com>
2026-03-03 08:53:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+ import time
+import sys
+
 alive == True
-  while True:
-    
+  while True:     
+    def typewriter(str, time):
+      for char in str
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(time)
+
+    def textCleanUp(input):
+      if input.isdigit():
+        retVal = "error no answer selected"
+      else:
+        retVal = input.upper()
+      return retVal
