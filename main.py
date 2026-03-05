3e639e61f9da3d6f6334a4e215ae6b9e3c7fe4a5
Zebulan the Red <zebpeak2023@gmail.com>
2026-03-05 08:59:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -195,15 +195,6 @@ def travel_event(player): # random event function
                     break
                 
 #>===============================================================================
-player = {
-    "name": "Ace",
-    "credits": 100,
-    "location": "Earth",
-    "fuel": 50,
-    "max_weight": 100,
-    "inventory": {"Tacos": 2, "Iron": 0, "Fuel": 5},
-    "alive": True
-}
 
 BASE_PRICES = {
     "Earth": {
