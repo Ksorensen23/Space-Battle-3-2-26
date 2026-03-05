8fe52179097b9d6021518fed142b6a1df79b1cfe
strange-Dyed <asiswick2028@jpkeefehs.org>
2026-03-05 10:51:19 -0500

    added default_player

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ player = {
     "alive": True
 }
 
+
+default_player = {                         
+    "name": "Ace",
+    "credits": 100,
+    "location": "Earth",
+    "fuel": 50,
+    "max_weight": 100,
+    "inventory": {"Tacos": 2, "Iron": 0, "Fuel": 5},
+    "alive": True
+    }
+
 # ==================== File Save/Load ====================
 def load_game():
     file_path = "player_save.json"
